d23f3a5978d3a5df94c85e1fd963b98c4d02f117
farouky <pharouky@gmail.com>
2022-08-19 01:22:08 +0100

    Clash Of Code 1 !!!

---
+++ Reversed.js
@@ -0,0 +1,14 @@
+// Given a number N, print N minus N where all digits have been reversed.
+
+// 2 -> (reversed) 2 -> 2-2 = 0
+// 57-> (reversed) 75 -> 57-75 = -18
+
+function reversed(number) {
+  let reverse = number.toString().split("").reverse().join("");
+  return number - reverse;
+}
+
+console.log(reversed(7)); // 0
+console.log(reversed(13)); // -18
+console.log(reversed(1751)); // 180
+console.log(reversed(9030)); // 8721
