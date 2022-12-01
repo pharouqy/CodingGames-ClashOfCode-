79def330b538c2d3e7c8c27f39314ab90a6bc29c
farouky <pharouky@gmail.com>
2022-12-01 01:31:39 +0100

    productOfSum

---
+++ productOfSum.js
@@ -0,0 +1,15 @@
+function productOfSum(string) {
+  const arr = string.split(" ");
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i].split("").reduce((a, b) => parseInt(a) + parseInt(b), 0);
+  }
+  return product;
+}
+
+console.log(productOfSum("1 25 3365 995645")); //4522
+console.log(productOfSum("1545 2455 3545365 995645455")); //386880
+console.log(productOfSum("545 454 65 6565 254545 3365 9945 455 645")); //106135029000
+console.log(productOfSum("1 25 4545 3365 995 645")); //738990
+console.log(productOfSum("1 25 33 65 9 95 645")); //873180
+console.log(productOfSum("1 25 3 365 995 645")); //101430
