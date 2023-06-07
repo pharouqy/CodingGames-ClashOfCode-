59e80acbc26b92d036980dcc12a79a8df4932de6
pharouqy <pharouqy@gmail.com>
2023-06-07 08:50:40 +0100

    teams

---
+++ teams.js
@@ -0,0 +1,181 @@
+/*Le mode de jeu est REVERSE : Vous n'avez pas accès à l'énoncé. Vous devez trouver quoi faire en observant les jeux de tests suivants :
+01 Test 1
+Entrée
+Sortie attendue
+Lisa, Frank, Lucy, Ann, Helen, Fran
+3
+Team 1: Lisa, Ann
+Team 2: Frank, Helen
+Team 3: Lucy, Fran
+02 Test 2
+Entrée
+Sortie attendue
+Wendell, Kandace, Rubin, Ezra, Candace, Lupe, Shurlocke, Carley, Cy, Valery, Lari, Isobel, Rhianon, Terrianne, Chriss, Si, Bonni, Israel, Giavani, Kip, Maurise, Aprilette, Sile, Billy, Raynard, Dierdre, Abdullah, Chane
+2
+Team 1: Wendell, Rubin, Candace, Shurlocke, Cy, Lari, Rhianon, Chriss, Bonni, Giavani, Maurise, Sile, Raynard, Abdullah
+Team 2: Kandace, Ezra, Lupe, Carley, Valery, Isobel, Terrianne, Si, Israel, Kip, Aprilette, Billy, Dierdre, Chane
+03 Test 3
+Entrée
+Sortie attendue
+Colette, Izak, Ozzie, Kinna, Kathlene, Cristobal, Joao, Torrin, Stearn, Ulric, Petronella, Carmelia, Brittany, Alexander, Goober, Frances, Porfirio, Israel, Waylin, Marcio, Allys, Wendell, Jabez, Stacee, Katalin
+3
+Team 1: Colette, Kinna, Joao, Ulric, Brittany, Frances, Waylin, Wendell, Katalin
+Team 2: Izak, Kathlene, Torrin, Petronella, Alexander, Porfirio, Marcio, Jabez
+Team 3: Ozzie, Cristobal, Stearn, Carmelia, Goober, Israel, Allys, Stacee
+04 Test 4
+Entrée
+Sortie attendue
+Kinna, Vannie, Adelina, Michaeline, Dylan, Tracey, Denny, Ferdy, Aurore, Elvera, Wendell, Quincey, Aprilette, Sile, Odysseus, Cleo, Lupe, Kingsly, Fairfax, Hammad, Bernie, Alpa, Elton, Stacee, Barron
+7
+Team 1: Kinna, Ferdy, Odysseus, Alpa
+Team 2: Vannie, Aurore, Cleo, Elton
+Team 3: Adelina, Elvera, Lupe, Stacee
+Team 4: Michaeline, Wendell, Kingsly, Barron
+Team 5: Dylan, Quincey, Fairfax
+Team 6: Tracey, Aprilette, Hammad
+Team 7: Denny, Sile, Bernie
+05 Test 5
+Entrée
+Sortie attendue
+Barron, Cristobal, Porfirio, Kathlene, Michaeline, Warden, Brear, Cameron, Carmelia, Alika, Kaila, Davon, Laverne, Aprilette, Caro, Israel, Lauren, Robt, Cy, Lynett, Adelina, Bonni, Shurlocke, Patrice, Cleo, Jabez
+5
+Team 1: Barron, Warden, Kaila, Israel, Adelina, Jabez
+Team 2: Cristobal, Brear, Davon, Lauren, Bonni
+Team 3: Porfirio, Cameron, Laverne, Robt, Shurlocke
+Team 4: Kathlene, Carmelia, Aprilette, Cy, Patrice
+Team 5: Michaeline, Alika, Caro, Lynett, Cleo
+06 Test 6
+Entrée
+Sortie attendue
+Kingsly, Brear, Mose, Terrianne, Frances, Powell, Olaf, Alpa, Abdullah, Joao, Vibhu, Rica, Marissa, Marcio, Webb, Roberta, Wesley, Lulita, Elvera
+4
+Team 1: Kingsly, Frances, Abdullah, Marissa, Wesley
+Team 2: Brear, Powell, Joao, Marcio, Lulita
+Team 3: Mose, Olaf, Vibhu, Webb, Elvera
+Team 4: Terrianne, Alpa, Rica, Roberta
+07 Test 7
+Entrée
+Sortie attendue
+Eve, Danette, Cleo, Brittany, Webb, Ozzie, Marshall, Robt, Powell, Adelina, Marcela, Aubrey, Brear, Tracey, Terrianne, Delmar, Roddy, Moe, Zachariah, Jabez, Maurise, Aprilette, Quincey, Charyl, Louis, Lauren, Mose
+8
+Team 1: Eve, Powell, Roddy, Louis
+Team 2: Danette, Adelina, Moe, Lauren
+Team 3: Cleo, Marcela, Zachariah, Mose
+Team 4: Brittany, Aubrey, Jabez
+Team 5: Webb, Brear, Maurise
+Team 6: Ozzie, Tracey, Aprilette
+Team 7: Marshall, Terrianne, Quincey
+Team 8: Robt, Delmar, Charyl
+08 Test 8
+Entrée
+Sortie attendue
+Carmelia, Petronella, Harland, Dolorita, Israel, Lulita, Rhianon, Terrianne, Laverne, Ez, Rita, Etti, Danette, Tiler, Rozella, Lian, Fionna, Torrin, Marcela, Davon, Aurore, Billy, Joann, Wolfy, Kinna, Roosevelt, Charyl, Patty, Roddy, Ozzie, Alika, Cleo, Sandye, Zachariah, Dierdre, Gilligan, Cameron, Fletch, Candace, Genaro, Marissa, Saunders, Stanley, Cy, Randee, Quincey, Kali, Raynard, Shirley, Isobel, Kristi, Kathlene, Wendeline, Ulric, Brittany, Marinna, Konstantin, Kandace, Zilvia, Pippy, Valery, Webb, Hunter, Tera, Marshall, Marcio, Cahra, Lupe, Theobald, Lynett, Rolf, Herculie, Aubrey, Kaila, Katalin, Chane, Elton, Teriann, Chriss, Aprilette
+14
+Team 1: Carmelia, Rozella, Roddy, Stanley, Konstantin, Rolf
+Team 2: Petronella, Lian, Ozzie, Cy, Kandace, Herculie
+Team 3: Harland, Fionna, Alika, Randee, Zilvia, Aubrey
+Team 4: Dolorita, Torrin, Cleo, Quincey, Pippy, Kaila
+Team 5: Israel, Marcela, Sandye, Kali, Valery, Katalin
+Team 6: Lulita, Davon, Zachariah, Raynard, Webb, Chane
+Team 7: Rhianon, Aurore, Dierdre, Shirley, Hunter, Elton
+Team 8: Terrianne, Billy, Gilligan, Isobel, Tera, Teriann
+Team 9: Laverne, Joann, Cameron, Kristi, Marshall, Chriss
+Team 10: Ez, Wolfy, Fletch, Kathlene, Marcio, Aprilette
+Team 11: Rita, Kinna, Candace, Wendeline, Cahra
+Team 12: Etti, Roosevelt, Genaro, Ulric, Lupe
+Team 13: Danette, Charyl, Marissa, Brittany, Theobald
+Team 14: Tiler, Patty, Saunders, Marinna, Lynett
+09 Test 9
+Entrée
+Sortie attendue
+Stanley, Marshall, Holly, Rita, Marissa, Shirley, Alika, Will, Carley, Raynard, Israel, Arnold, Elton, Dierdre, Aprilette, Joann, Ulric, Vincent, Genaro, Davon, Teodoor, Alpa, Kinna, Jabez, Allin, Sybille, Edgar, Izak, Caro, Quincey, Katalin, Maurise, Colette, Allys, Pippy, Moishe, Lynett, Wolfy, Grier, Brear, Warden, Billy, Aurore, Cassi, Shumeet, Dedra, Elvera, Cameron, Vannie, Juditha, Clinten, Kip, Charyl, Melesa, Patty, Isobel, Powell, Lulita, Dolorita, Alexander, Adelina, Fletch, Chane, Ambrosius, Shurlocke, Shalna, Lian, Kali, Gilligan, Tiler, Odysseus
+19
+Team 1: Stanley, Davon, Grier, Lulita
+Team 2: Marshall, Teodoor, Brear, Dolorita
+Team 3: Holly, Alpa, Warden, Alexander
+Team 4: Rita, Kinna, Billy, Adelina
+Team 5: Marissa, Jabez, Aurore, Fletch
+Team 6: Shirley, Allin, Cassi, Chane
+Team 7: Alika, Sybille, Shumeet, Ambrosius
+Team 8: Will, Edgar, Dedra, Shurlocke
+Team 9: Carley, Izak, Elvera, Shalna
+Team 10: Raynard, Caro, Cameron, Lian
+Team 11: Israel, Quincey, Vannie, Kali
+Team 12: Arnold, Katalin, Juditha, Gilligan
+Team 13: Elton, Maurise, Clinten, Tiler
+Team 14: Dierdre, Colette, Kip, Odysseus
+Team 15: Aprilette, Allys, Charyl
+Team 16: Joann, Pippy, Melesa,
+Team 17: Ulric, Moishe, Patty,
+Team 18: Vincent, Lynett, Isobel,
+Team 19: Genaro, Wolfy, Powel'*/
+
+function teams(string, number) {
+  const array = string.split(", ");
+  const numberOfTeams = array.length / number;
+  const arrOfTeams = [];
+  for (let i = 0; i < numberOfTeams; i++) {
+    arrOfTeams.push(array.slice(number * i, number * (i + 1)));
+  }
+  const result = [];
+  for (let i = 0; i < number; i++) {
+    const teamMembers = [];
+    for (let j = 0; j < arrOfTeams.length; j++) {
+      if (arrOfTeams[j][i]) {
+        teamMembers.push(arrOfTeams[j][i]);
+      }
+    }
+    result.push(`Team ${i + 1}: ${teamMembers.join(", ")}`);
+  }
+  return result;
+}
+
+console.log(teams("Lisa, Frank, Lucy, Ann, Helen, Fran", 3)); // [ 'Team 1: Lisa, Ann', 'Team 2: Frank, Helen', 'Team 3: Lucy, Fran' ]
+console.log(
+  teams(
+    "Wendell, Kandace, Rubin, Ezra, Candace, Lupe, Shurlocke, Carley, Cy, Valery, Lari, Isobel, Rhianon, Terrianne, Chriss, Si, Bonni, Israel, Giavani, Kip, Maurise, Aprilette, Sile, Billy, Raynard, Dierdre, Abdullah, Chane",
+    2
+  )
+);
+console.log(
+  teams(
+    "Colette, Izak, Ozzie, Kinna, Kathlene, Cristobal, Joao, Torrin, Stearn, Ulric, Petronella, Carmelia, Brittany, Alexander, Goober, Frances, Porfirio, Farouk, Waylin, Marcio, Allys, Wendell, Jabez, Stacee, Katalin",
+    3
+  )
+);
+console.log(
+  teams(
+    "Kinna, Vannie, Adelina, Michaeline, Dylan, Tracey, Denny, Ferdy, Aurore, Elvera, Wendell, Quincey, Aprilette, Sile, Odysseus, Cleo, Lupe, Kingsly, Fairfax, Hammad, Bernie, Alpa, Elton, Stacee, Barron",
+    7
+  )
+);
+console.log(
+  teams(
+    "Barron, Cristobal, Porfirio, Kathlene, Michaeline, Warden, Brear, Cameron, Carmelia, Alika, Kaila, Davon, Laverne, Aprilette, Caro, Israel, Lauren, Robt, Cy, Lynett, Adelina, Bonni, Shurlocke, Patrice, Cleo, Jabez",
+    5
+  )
+);
+console.log(
+  teams(
+    "Kingsly, Brear, Mose, Terrianne, Frances, Powell, Olaf, Alpa, Abdullah, Joao, Vibhu, Rica, Marissa, Marcio, Webb, Roberta, Wesley, Lulita, Elvera",
+    4
+  )
+);
+console.log(
+  teams(
+    "Eve, Danette, Cleo, Brittany, Webb, Ozzie, Marshall, Robt, Powell, Adelina, Marcela, Aubrey, Brear, Tracey, Terrianne, Delmar, Roddy, Moe, Zachariah, Jabez, Maurise, Aprilette, Quincey, Charyl, Louis, Lauren, Mose",
+    8
+  )
+);
+console.log(
+  teams(
+    "Carmelia, Petronella, Harland, Dolorita, Israel, Lulita, Rhianon, Terrianne, Laverne, Ez, Rita, Etti, Danette, Tiler, Rozella, Lian, Fionna, Torrin, Marcela, Davon, Aurore, Billy, Joann, Wolfy, Kinna, Roosevelt, Charyl, Patty, Roddy, Ozzie, Alika, Cleo, Sandye, Zachariah, Dierdre, Gilligan, Cameron, Fletch, Candace, Genaro, Marissa, Saunders, Stanley, Cy, Randee, Quincey, Kali, Raynard, Shirley, Isobel, Kristi, Kathlene, Wendeline, Ulric, Brittany, Marinna, Konstantin, Kandace, Zilvia, Pippy, Valery, Webb, Hunter, Tera, Marshall, Marcio, Cahra, Lupe, Theobald, Lynett, Rolf, Herculie, Aubrey, Kaila, Katalin, Chane, Elton, Teriann, Chriss, Aprilette",
+    14
+  )
+);
+console.log(
+  teams(
+    "Stanley, Marshall, Holly, Rita, Marissa, Shirley, Alika, Will, Carley, Raynard, Israel, Arnold, Elton, Dierdre, Aprilette, Joann, Ulric, Vincent, Genaro, Davon, Teodoor, Alpa, Kinna, Jabez, Allin, Sybille, Edgar, Izak, Caro, Quincey, Katalin, Maurise, Colette, Allys, Pippy, Moishe, Lynett, Wolfy, Grier, Brear, Warden, Billy, Aurore, Cassi, Shumeet, Dedra, Elvera, Cameron, Vannie, Juditha, Clinten, Kip, Charyl, Melesa, Patty, Isobel, Powell, Lulita, Dolorita, Alexander, Adelina, Fletch, Chane, Ambrosius, Shurlocke, Shalna, Lian, Kali, Gilligan, Tiler, Odysseus",
+    19
+  )
+);
