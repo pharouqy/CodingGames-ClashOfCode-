6f72225f6fb1c8796b0aabd70ac0e6d68389cfc4
pharouqy <pharouqy@gmail.com>
2023-06-15 21:14:57 +0100

    sumOfIntegers

--- extractName.js
+++ extractName.js
@@ -13,8 +13,8 @@ Sortie
 Hamzai Otala*/
 
 function extractName(string) {
-    return string.match(/[A-Za-z ]/g).join("");
+  return string.match(/[A-Za-z ]/g).join("");
 }
 
-console.log(extractName("H2a3m12z13a4i O45t67a78l8a")) // Hamzai Otala
-console.log(extractName("F4575a5r4587o6325u54k Y883201o4763u45n45si")) // Farouk Younsi
+console.log(extractName("H2a3m12z13a4i O45t67a78l8a")); // Hamzai Otala
+console.log(extractName("F4575a5r4587o6325u54k Y883201o4763u45n45si")); // Farouk Younsi
