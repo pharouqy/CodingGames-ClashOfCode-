153d700e76a6fca8d15b4be95cfdf35087d1af0e
pharouqy <pharouky@gmail.com>
2024-08-08 22:46:01 +0100

    ASCIISum

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-/*Le mode de jeu est REVERSE : Vous n'avez pas accès à l'énoncé. Vous devez trouver quoi faire en observant les jeux de tests suivants :
-01 Test 1
+/*Given a string, output the sum of each ASCII character value multiplied by its index in the string
 Entrée
-Sortie attendue
-1
-1
-02 Test 2
-Entrée
-Sortie attendue
-10
-12345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910
-03 Test 3
-Entrée
-Sortie attendue
-12
-123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112
-04 Test 4
-Entrée
-Sortie attendue
-6
-123456123456123456123456123456123456*/
+Line 1: A string S
+Sortie
+Line 1: The sum of each ASCII character value multiplied by its index.
 
-function productOfn(n) {
-  const array = [];
-  for (let i = n; i > 0; i--) {
-    array.push(i);
-  }
-  return array.reverse("").join("").repeat(n);
+Example: ABC => 65*0 + 66*1 + 67*2 => 200
+Contraintes
+The string contains no space.
+Exemple
+Entrée
+ABC
+Sortie
+200*/
+
+function ASCIISum(string) {
+  const array = string.split("");
+  return array
+    .map((lettre, index) => {
+      return lettre.charCodeAt(0) * index;
+    })
+    .reduce((current, previous) => {
+      return current + previous;
+    }, 0);
 }
 
-console.log(productOfn(1)); // 1
-console.log(productOfn(10)); // 12345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910
+console.log(ASCIISum("ABC")); // 200
+console.log(ASCIISum("aaaa")); // 582
+console.log(ASCIISum("aboABC")); // 1114
+console.log(ASCIISum("123")); // 152
+console.log(ASCIISum("FaRoUk")); // 1469
